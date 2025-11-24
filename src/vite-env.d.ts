e4f692c63d46f7d3401c2eb169fdd0b307503735
Wentao <jwt625@gmail.com>
2025-11-23 17:16:16 -0800

    add debug to prod

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
 	readonly VITE_DEBUG: string;
 	readonly VITE_MAX_POLYGONS_PER_RENDER: string;
 	readonly VITE_FPS_UPDATE_INTERVAL: string;
+	readonly VITE_SIGNALING_SERVER_URL: string;
+	readonly VITE_SIGNALING_SERVER_TOKEN: string;
 }
 
 interface ImportMeta {
